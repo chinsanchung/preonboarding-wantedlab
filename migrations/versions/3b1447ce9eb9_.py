e8898ce8413ced94d57542f2ec940ef4a38fe20d
Chinsan Chung <chinsanchung@gmail.com>
2021-12-14 01:58:52 +0900

    chore: 데이터베이스 변경
    - 데이터베이스를 SQLite3에서 AWS RDS for MySQL으로 수정했습니다.
    - 그 이유는 과제의 대부분을 SQLite3으로 했기에 다른 것을 하고 싶었던 것과 AWS로 EC2, RDS를 직접 설정하고 싶언던 점 때문입니다.
    - python-dotenv, pymysql 을 설치했습니다.
    
    resolves: #2

--- migrations/versions/3b1447ce9eb9_.py
+++ migrations/versions/3b1447ce9eb9_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: a17be6e3550f
+Revision ID: 3b1447ce9eb9
 Revises: 
-Create Date: 2021-12-12 21:31:01.868169
+Create Date: 2021-12-14 01:49:08.543409
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = "a17be6e3550f"
+revision = "3b1447ce9eb9"
 down_revision = None
 branch_labels = None
 depends_on = None
